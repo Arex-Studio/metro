907f685ad70f8da0bf326a8b3216441c293ccdfc
Arex360 <49303647+Arex360@users.noreply.github.com>
2022-02-09 02:00:49 -0800

    changes

--- src/components/TransparentBox.jsx
+++ src/components/TransparentBox.jsx
@@ -16,14 +16,16 @@ let TransparentBox = ()=>{
                 </div>
             </div>
             <div className="row1">
-                <Selector options={['Buy']}/>
-                <Selector options={['Completion Status']}/>
-                <div className="inputfield">
-                    <input type="text" placeholder="Location, Community , City"/>
+                <div className="rowitem1">
+                    <Selector options={['Select your option']}/>
                 </div>
-                <div className="button">
-                    <div className="fill"></div>
-                    <h2>Find</h2>
+                <div className="rowitem2">
+                    <Selector options={['Min. Area']}/>
+                    <Selector options={['Max. Area']}/>
+                    <Selector options={['Min. Bedrooms']}/>
+                    <Selector options={['Max. Bedrooms']}/>
+                    <Selector options={['Min. Price']}/>
+                    <Selector options={['Max. Price']}/>
                 </div>
             </div>
         </div>
