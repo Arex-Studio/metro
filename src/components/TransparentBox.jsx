12e9f47e3998ccbca2815e09b4876ff65ad7af61
Arex360 <49303647+Arex360@users.noreply.github.com>
2022-02-09 01:20:42 -0800

    v2

---
+++ src/components/TransparentBox.jsx
@@ -0,0 +1,8 @@
+let TransparentBox = ()=>{
+    return(
+        <div className="transparent">
+            <h1>Find Property in Dubai</h1>
+        </div>
+    )
+}
+export default TransparentBox
--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -1,10 +1,13 @@
 import background from '../assets/hero.jpg'
 import NavBar from '../components/Navbar'
+import TransparentBox from '../components/TransparentBox'
+import '../style/hero.css'
 let HeroSection = ()=>{
     return(
         <section className="hero">
             <img src={background} alt="" id="heroimg" />
             <NavBar/>
+            <TransparentBox/>
         </section>
     )
 }
